8db093591c997d058fdb4b188ac32d87d2f9fb18
Yogafigi <putatroktok6@gmail.com>
2023-02-24 21:23:12 +0700

    creatingblog

---
+++ js/blog.js
@@ -0,0 +1,111 @@
+// object
+// const isHere = {
+//   name: " Yoga",
+//   age: 21,
+//   address: "Cirebon",
+// };
+
+// console.table(isHere);
+
+//array
+
+// const isName = ["Yoga", 21, "Cirebon"];
+// console.log(isName);
+
+//array of object
+
+// const myClass = [
+//   {
+//     day: "kamis",
+//     student: 54,
+//   },
+// ];
+
+// const form = document.querySelector("form");
+datas = [];
+
+const getData = (event) => {
+  event.preventDefault();
+  let projectName = document.getElementById("projectname").value;
+  let startDate = document.getElementById("startdate").value;
+  let endDate = document.getElementById("enddate").value;
+  let description = document.getElementById("description").value;
+  let img = document.getElementById("blog-img").files;
+
+  let nodejs = document.getElementById("nodejs").checked;
+  let nextjs = document.getElementById("nextjs").checked;
+  let reactjs = document.getElementById("reactjs").checked;
+  let typescript = document.getElementById("typescript").checked;
+
+  let newNodejs =
+    nodejs == true
+      ? `<img id="nodejs" src="assets/image/nodejs.png" alt="">`
+      : "";
+  let newNextjs =
+    nextjs == true
+      ? `<img id="nextjs" src="assets/image/nextjs.png" alt="" style="width:23px">`
+      : "";
+  let newReactjs =
+    reactjs == true
+      ? `<img id="reactjs" src="assets/image/reactjs.png" alt="" style="width:28px;">`
+      : "";
+  let newTypescript =
+    typescript == true
+      ? `<img id="typescript" src="assets/image/typescript.png" alt="" style="width:22px">`
+      : "";
+
+  img = URL.createObjectURL(img[0]);
+
+  if (projectName == "") {
+    return alert("Project Name tidak Boleh Kosong");
+  } else if (description == "") {
+    return alert("Description Name tidak Boleh Kosong");
+  }
+
+  let data = {
+    projectName,
+    startDate,
+    endDate,
+    description,
+    img,
+    newNodejs,
+    newNextjs,
+    newReactjs,
+    newTypescript,
+  };
+
+  datas.push(data);
+  console.log(datas);
+  showData();
+};
+
+const showData = () => {
+  document.getElementById("contents").innerHTML = "";
+
+  for (i = 0; i < datas.length; i++) {
+    document.getElementById("contents").innerHTML += `
+        
+        <div class="card-post">
+            <img class="post-img" src="${datas[i].img}">
+            <div class="title-date">
+                <h1 class="title">${datas[i].projectName}</h1>
+                <h3 class="date">Durasi : 2 bulan</h3>
+            </div>
+            <div class="post-content">
+                <p>${datas[i].description}</p>
+            </div>
+            <div class="tag-tech" id="tag-tech">
+                ${datas[i].newNodejs}
+                ${datas[i].newNextjs}
+                ${datas[i].newReactjs}
+                ${datas[i].newTypescript}
+            </div>
+            <div class="post-button">
+                <button class="edit">Edit</button>
+                <button class="delete">Delete</button>
+            </div>
+        </div>
+        `;
+  }
+  alert("Sukses masukan data");
+};
